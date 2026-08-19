739e0f955ef78e64f74d5760319ce21bcdbbcadb
Matheus José Silva Miguel <matheus29150@gmail.com>
2026-08-18 23:04:29 -0300

    Update script.js

--- www/script.js
+++ www/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://192.168.1.50:3000/api";
+const API_URL = "https://focuslist-server-us0y.onrender.com/";
 
 /* ======================================================
    LOGIN / CADASTRO
